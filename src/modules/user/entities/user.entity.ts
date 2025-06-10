f46144382e7b569fbf62e471fcafa627f64b4cd0
Ronal <ronal.sanx21@gmail.com>
2025-06-10 18:57:25 -0500

    Diseño Entidad Usuario / DTO

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,1 +1,23 @@
-export class User {}
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
+
+@Entity()
+export class User {
+
+    @PrimaryColumn()
+    id: string
+
+    @Column()
+    name: string
+
+    @Column()
+    email: string
+
+    @Column()
+    password: string
+
+    @Column()
+    role: string
+
+
+}
