82aeef5608bca9e096e3dcbe23c65fd514ce322c
Ronal <ronal.sanx21@gmail.com>
2025-06-10 16:27:18 -0500

    Conexion base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 import { CustomersModule } from './modules/customers/customers.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
-  imports: [AuthModule, CustomersModule],
+  imports: [
+    
+    TypeOrmModule.forRoot({
+      host: process.env.DBHOST || 'localhost',
+      port: 5432,
+      username: process.env.USERNAMEDB || 'userpassword',
+      password: process.env.PASSWORDDB || 'password',
+      database: process.env.NAMEDB || 'venticrm',
+      type:'postgres'
+    }),
+    AuthModule, CustomersModule],
   controllers: [],
   providers: [],
 })
