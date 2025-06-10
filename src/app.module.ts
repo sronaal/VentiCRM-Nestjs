6c8c6137112981efe99102b2ef004eacfd67cf33
Ronal <ronal.sanx21@gmail.com>
2025-06-10 16:03:35 -0500

    Creacion Modulo customers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
+import { CustomersModule } from './modules/customers/customers.module';
 
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, CustomersModule],
   controllers: [],
   providers: [],
 })
