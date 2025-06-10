9f8cf8974e657bd54166e1815acbf73a6e0083e3
Ronal <ronal.sanx21@gmail.com>
2025-06-10 18:58:26 -0500

    Servicio creacion usuario

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,12 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { v4 as uuid } from 'uuid'
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+
+
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>) { }
+
+  async create(createUserDto: CreateUserDto) {
+
+
+    let userFind = await this.userRepository.findOne({ where: { email: createUserDto.email } })
+
+    if (userFind) { return new HttpException(`El usuario con el correo ${createUserDto.email} ya se encuentra registrado`, HttpStatus.CONFLICT) }
+
+
+    let userCreate = this.userRepository.create({
+      id: uuid(),
+      name: createUserDto.name,
+      email: createUserDto.email,
+      password: createUserDto.password
+    })
+
+    this.userRepository.save(userCreate)
+    .then((usuario) => {
+      
+      return new HttpException(`Usuario creado: ${usuario}`, HttpStatus.CREATED)
+    })
+    .catch((error) => {
+      console.log(error)
+      return new HttpException(`Ha ocurrido un error ${error}`, HttpStatus.INTERNAL_SERVER_ERROR)
+    })
+
+
+
   }
+
+
+
 
   findAll() {
     return `This action returns all user`;
