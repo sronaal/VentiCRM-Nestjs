e7b4665ef86727bddee7b2e096b321efc92494d2
Ronal <ronal.sanx21@gmail.com>
2025-06-10 19:04:48 -0500

    Validaciones create user DTO

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,6 +1,17 @@
+import { IsEmail, IsNotEmpty, IsString } from "class-validator"
+
 export class CreateUserDto {
 
+    @IsString()
+    @IsNotEmpty({message: 'El nombre es obligatorio'})
     name: string
+    
+    @IsString()
+    @IsEmail()
+    @IsNotEmpty({message: 'El correo es obligatorio'})
     email: string
+
+    @IsString()
+    @IsNotEmpty({message: 'La contraseña es obligatoria'})
     password: string
 }
